4c75254f07c17ddfcc82c95f66d3b5f5376271f8
Jochen Neubeck <jochen.neubeck.ext@siemens-energy.com>
2025-06-22 21:29:31 +0200

    WIP

--- build.bat
+++ build.bat
@@ -18,7 +18,7 @@ REM sh build_ffmpeg.sh x86 || EXIT /B 1
 REM MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
 REM IF ERRORLEVEL 1 EXIT /B 1
 
-sh build_ffmpeg_msvc.sh x64 || EXIT /B 1
+sh build_ffmpeg.sh x64 || EXIT /B 1
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=x64
 IF ERRORLEVEL 1 EXIT /B 1
 
