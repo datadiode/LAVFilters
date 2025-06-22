8ceca94a8642055566f55d288be4ec1c91ec8496
Jochen Neubeck <jochen.neubeck.ext@siemens-energy.com>
2025-06-22 18:32:38 +0200

    WIP

--- build.bat
+++ build.bat
@@ -1,18 +1,22 @@
-@ECHO OFF
+@ECHO ON
 
 for /f "usebackq tokens=*" %%i in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version "[16.0,18.0)" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-  set VSInstallDir=%%i
+  set "VSInstallationPath=%%i"
 )
 
 set "VSCMD_START_DIR=%CD%"
-call "%VSInstallDir%\Common7\Tools\VsDevCmd.bat" -no_logo -arch=x86 > NUL
+call "%VSInstallationPath%\Common7\Tools\VsDevCmd.bat" -no_logo -arch=x86 > NUL
+path
+where cl.exe
+where gcc.exe
+gcc --version
 
-sh build_ffmpeg.sh x86 || EXIT /B 1
-MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
-IF ERRORLEVEL 1 EXIT /B 1
+REM sh build_ffmpeg.sh x86 || EXIT /B 1
+REM MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=Win32
+REM IF ERRORLEVEL 1 EXIT /B 1
 
 sh build_ffmpeg.sh x64 || EXIT /B 1
 MSBuild.exe LAVFilters.sln /nologo /m /t:Rebuild /property:Configuration=Release;Platform=x64
 IF ERRORLEVEL 1 EXIT /B 1
 
-PAUSE
+REM PAUSE
